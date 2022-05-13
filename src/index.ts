aaa194ed57669bc4a0d8a6a7db59c697eab09a04
sam bacha <sam@manifoldfinance.com>
2022-05-13 06:16:35 -0700

    Prefer Named Exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
-export * from './events';
-// export { EventEmitterWrapper } from './events';
+// export * from './events';
+export { EventEmitterWrapper } from './events';
 // both styles of export have no impact on output of dist
+// However we should Prefer Named Exports as opposed to defaults, see README for more info
