e6c97646f3dac47d2eea2019378a864d5651d7f9
sam bacha <sam@manifoldfinance.com>
2022-05-22 15:27:45 -0700

    feat(pipelines): release workflow (#1)

---
+++ .prettierrc.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = {
+  arrowParens: 'always',
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
+  printWidth: 100,
+  proseWrap: 'always',
+  quoteProps: 'as-needed',
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+};
