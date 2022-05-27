19e4e7dd1410e1b5e7b1d0e83205706731d96149
sam bacha <sam@manifoldfinance.com>
2022-05-27 09:24:48 -0700

    feat: abstract IEvents

--- src/events/index.ts
+++ src/events/index.ts
@@ -169,3 +169,13 @@ export default class SafeEventEmitter extends EventEmitter {
     return true;
   }
 }
+
+export abstract class IEvents {
+  public abstract events: EventEmitter;
+
+  // events
+  public abstract on(event: string, listener: any): void;
+  public abstract once(event: string, listener: any): void;
+  public abstract off(event: string, listener: any): void;
+  public abstract removeListener(event: string, listener: any): void;
+}
